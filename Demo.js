cd00d33c6fd86f0b9b4053d3aa433b56c7ffd137
Priyanka sharma <150547532+Piyasharmacoder@users.noreply.github.com>
2024-09-22 13:18:24 +0530

    add files

---
+++ Demo.js
@@ -0,0 +1,25 @@
+const accountId = 101;
+let accountEmail = "poia@gmail.com";
+var accountpassword = "12345";
+accountAdresss = "indore";
+let accountsate;
+let name =  "Ram";
+
+//accountId=2;//  this is not allowed beacuse const variable is constant this is not changeable 
+
+
+
+console.error();
+accountEmail = "Riya@gmail.com"
+accountpassword = " 12121212"
+accountAdresss= " bhopal"
+
+console.log(accountId);
+console.table([ accountId, accountEmail, accountpassword, accountAdresss,accountsate])
+console.groupCollapsed([ name,accountEmail,accountId, accountpassword, accountsate])
+//console.clear();
+
+
+
+
+
